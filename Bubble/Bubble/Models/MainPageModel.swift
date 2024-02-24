f6282f59587b853ffc402622ead5062080cd326a
Gabrielle Stewart <159938739+gabrielleestewart@users.noreply.github.com>
2024-02-24 18:48:31 -0500

    Delete Bubble directory

--- Bubble/Bubble/Models/MainPageModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainPageModel.swift
-//  Bubble
-//
-//  Created by Gabrielle Stewart on 2/24/24.
-//
-
-import Foundation
--- Bubble/Bubble/View_Model/LoginModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginModel.swift
-//  Bubble
-//
-//  Created by Savv Lin on 2/24/24.
-//
-
-import Foundation
