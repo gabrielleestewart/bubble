1578487a5eecaba430b89616795b372b1f201d03
Melanie Andrade-Muñoz <xvoqt@unc.edu>
2024-02-24 17:42:10 -0500

    Update User.swift

--- Bubble/Bubble/Model/User.swift
+++ Bubble/Bubble/Model/User.swift
@@ -2,7 +2,27 @@
 //  User.swift
 //  Bubble
 //
-//  Created by Savv Lin on 2/24/24.
+//  Created by Mel Andrade-Muñoz on 2/24/24.
 //
 
 import Foundation
+
+struct User: Identifiable, Codable, Hashable {
+    let id: String
+    let fullname: String
+    let email: String
+    let username: String
+    var profileImageUrl: String?
+    let bio: String?
+    
+    // added this entire init part below
+    init(id: String, fullname: String, email: String, username: String, profileImageUrl: String? = nil, bio: String? = nil) {
+        self.id = id
+        self.fullname = fullname
+        self.email = email
+        self.username = username
+        self.profileImageUrl = profileImageUrl
+        self.bio = bio
+    }
+}
+
