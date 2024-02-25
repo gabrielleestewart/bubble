99f503fdeb772eabc3956eeeabbb2e9b2ce00340
Gabrielle Stewart <159938739+gabrielleestewart@users.noreply.github.com>
2024-02-25 01:15:42 -0500

    ombre added to background

--- Bubble/Bubble/Views/ObFeatureView.swift
+++ Bubble/Bubble/Views/ObFeatureView.swift
@@ -11,48 +11,52 @@ struct ObFeatureView: View {
     var page: Page
     
     @State private var isAnimating: Bool = false
-    
-    
-    var body: some View {
-      ZStack {
-        
-        VStack(spacing: 20) {
-          // IMAGE
-          Image(page.image)
-            .resizable()
-            .scaledToFit()
-            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
-            .scaleEffect(isAnimating ? 1.0 : 0.6)
-            .padding(.horizontal)
-            .padding(.horizontal)
 
-          // HEADLINE
-          Text(page.headline)
-            .foregroundColor(Color.white)
-            .multilineTextAlignment(.center)
-            .padding(.horizontal, 16)
-            .frame(maxWidth: 480)
-            .padding()
-            .padding(.vertical)
-          
-          // BUTTON: START
-          StartButtonView()
-        } //: VSTACK
-      } //: ZSTACK
-      .onAppear {
-        withAnimation(.easeOut(duration: 0.5)) {
-          isAnimating = true
-        }
-      }
-      .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.white, Color("coral"), Color(.white)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                // IMAGE
+                Image(page.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                    .padding(.horizontal)
+                    
+                // HEADLINE
+                Text(page.headline)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                    .padding()
+                    .padding(.vertical)
+                
+                // BUTTON: START
+                StartButtonView()
+            }
+            .onAppear {
+                withAnimation(.easeOut(duration: 0.5)) {
+                    isAnimating = true
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
             .background(LinearGradient(gradient: Gradient(colors: page.Colors), startPoint: .top, endPoint: .bottom))
             .cornerRadius(20)
             .padding(.horizontal, 20)
+        }
     }
 }
-
-struct ObFeatureView_Previews: PreviewProvider {
-    static var previews: some View {
-        ObFeatureView(page: ObData[0])
+    
+    struct ObFeatureView_Previews: PreviewProvider {
+        static var previews: some View {
+            ObFeatureView(page: ObData[0])
+        }
     }
-}
--- Bubble/Bubble/Views/OnboardingView.swift
+++ Bubble/Bubble/Views/OnboardingView.swift
@@ -7,27 +7,51 @@
 
 import SwiftUI
 
+struct PageControl: View {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<3) { page in
+                Circle()
+                    .fill(page == currentPage ? Color.white : Color.gray)
+                    .frame(width: 8, height: 8)
+            }
+        }
+    }
+}
+
 struct OnBoardingView: View {
     // MARK: - PROPERTIES
     
     var pages: [Page] = ObData
-
+    @State private var selectedPageIndex: Int = 0
+    
     // MARK: - BODY
     
     var body: some View {
-      TabView {
-        ForEach(pages[0...2]) { item in
-          ObFeatureView(page: item)
-        } //: LOOP
-      } //: TAB
-      .tabViewStyle(PageTabViewStyle())
-      .padding(.vertical, 20)
+        ZStack {
+            TabView(selection: $selectedPageIndex) {
+                ForEach(pages.indices, id: \.self) { index in
+                    ObFeatureView(page: pages[index])
+                        .tag(index)
+                }
+            } //: TAB
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .padding(.vertical, 20)
+            
+            VStack {
+                Spacer()
+                PageControl(numberOfPages: pages.count, currentPage: $selectedPageIndex)
+                .padding(.bottom, 40)
+            }
+        }
     }
-
-}
-
-struct OnBoardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnBoardingView()
+    
+    struct OnBoardingView_Previews: PreviewProvider {
+        static var previews: some View {
+            OnBoardingView()
+        }
     }
 }
