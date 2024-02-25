90eaa3d52eb7bbe547ab1ffac05773e0ab6d4a60
kencam <kencam@unc.edu>
2024-02-25 01:42:55 -0500

    settingsview upload test

---
+++ Bubble/Bubble/Views/SettingsView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingView.swift
+//  Bubble
+//
+//  Created by Kennedy Cameron on 2/25/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        var header = ["My Profile","Language", "About", "Log Out"]
+        //var
+        //put here
+        
+        
+        
+        
+        
+        
+    }
+}
+
+
+
+struct SettingView_Preview: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
